673b03867aa825366b8c8a0666f1e9d693f25a7c
1t1sCooL <malabugin@anykey.pro>
2025-12-05 15:40:47 +0300

    add validation

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,7 +2,7 @@ import './styles/global.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import {MainPage} from "../pages/MainPage";
+import {MainPage} from "@/pages/MainPage";
 
 function App() {
   return (
--- src/shared/ui/svg/index.ts
+++ src/shared/ui/svg/index.ts
@@ -1,1 +1,3 @@
-export { Svg } from './ui/Svg.tsx'
+export { Facebook } from './ui/Facebook.tsx'
+export { Twitter } from './ui/Twitter.tsx'
+export { Instagram } from './ui/Instagram.tsx'
